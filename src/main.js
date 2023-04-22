725bc14c458c20f01bf6114bb8d0bbee148431e7
Rocca-Francesco <roccafrance98@yahoo.it>
2023-04-22 14:44:51 +0200

    dependencies base

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+
+// bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App, bootstrap).mount('#app')
